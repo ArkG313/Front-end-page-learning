ad7f3a6e8b6a21b40ff0eb01106ceb95d81dcc84
ArkG313 <1403873255@qq.com>
2025-02-26 22:44:16 +0800

    第三次修改

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('Hello, world!');
+console.log(window);
+console.log(document);
+console.log(document.getElementsByClassName("sign-in"));
+var btn=document.getElementById("btn");
+btn.addEventListener("click",function() {
+    console.log("欢迎光临");
+    // window.alert("登陆成功");
+    // window.confirm("你好你好你好");
+    // document.getElementById("username")
+    console.log(document.getElementById("username").value);
+    // document.getElementById("password");
+    console.log( document.getElementById("password").value);
+    // window.location.href="/index.html";
+
+    
+})
+// var js=document.createElement("script");
+// js.src="index.js";
+// document.body.appendChild(js);
